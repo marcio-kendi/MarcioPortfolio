69950c25cfb6878fdd1009ef3d485566c007fb84
Marcio Iwamoto <kendi_marcio@hotmail.com>
2021-11-04 23:19:21 +1300

    title change

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={[styles.main]}>
         <h1 className={styles.title}>
-          Welcome to Marcio's portfolio
+          Marcio's portfolio
         </h1>
         <hr className={['container']} />
         <div className={['container']}>
