c0543c3e27ecfe14950465ed0e87e2d8b6dc235c
Marcio Iwamoto <kendi_marcio@hotmail.com>
2021-11-05 19:33:37 +1300

    menu added with components

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,27 @@
 import styles from '../styles/Home.module.scss'
+import React, { Component } from 'react';
+import Modal from '../components/Modal/Modal.js'
 
-export function Button({menuName, onclick}) {
-  return (
-  <span className={['col-3 text-center d-flex justify-content-center']}>
-    <span className={styles.btn} onClick={onclick}>
-      <h3>{menuName}</h3>
-    </span>
-  </span>
-  )
-}
+export default class Button extends Component {
+  state = {
+    show: false
+  };
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <span className={['col-3 text-center']}>
+        <span className={styles.btn} onClick={e => {
+            this.showModal(e);
+          }}>
+          <h3>{this.props.menuName}</h3>
+        </span>
+        <Modal onClose={this.showModal} show={this.state.show} modalTitle={this.props.modaltitle} selectedMenu={this.props.selectedmenu} />
+      </span>
+    )
+  }
+};
