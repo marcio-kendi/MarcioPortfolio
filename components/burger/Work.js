ca82f9a991e3ab7620a75c475b62bd747ccfa3a7
Marcio Iwamoto <kendi_marcio@hotmail.com>
2021-11-19 15:56:24 +1300

    trial iframe

--- components/burger/Work.js
+++ components/burger/Work.js
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom";
 import styles from '../../styles/Home.module.scss'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import FirstFile from '../workList/DairyWorks/DisplayBanners/300x250/index.html'
+import { auto } from '@popperjs/core';
 
 function IFrame({ children }) {
   const [ref, setRef] = React.useState();
@@ -15,13 +15,20 @@ function IFrame({ children }) {
     </iframe>
   );
 }
+// const iframe = {
+//   margin: 0,
+//   width: 'inherit',
+//   display: 'flex',
+// }
 
 const Work = () => {
   return (
     
   <Carousel className={styles.carouselRoot}>
     <div className={styles.menu}>
-      <iframe src="../workList/DairyWorks/DisplayBanners/300x250/index.html" />
+        <iframe width="300px" height="600px" scrolling="no" src="./workList/DairyWorks/DisplayBanners/300x600/index.html" />
+        <iframe width="300px" height="250px" scrolling="no" src="./workList/DairyWorks/DisplayBanners/300x250/index.html" />
+        <iframe width="970px" height="250px" scrolling="no" src="./workList/DairyWorks/DisplayBanners/970x250/index.html" />
     </div>
   </Carousel>
   )
---
+++ public/workList/DairyWorks/DisplayBanners/300x250/Test.js
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+function Avatar() {
+  return <iframe src="/index.html" alt="me" width="100" height="100" />
+}
+
+export default Avatar
